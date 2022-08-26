408dbf7a1534d7269dbd92501480ce1a58c9d604
GuoCY <67821713+JamKuo@users.noreply.github.com>
2022-08-27 01:46:20 +0800

    add

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220827.014306';
+const cacheName = 'chirpy-20220827.014442';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
